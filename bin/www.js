9ebf8950ef61113afc4ede85530d9f49ae09d623
JerryLin <jerrydotlam@gmail.com>
2018-12-09 21:52:46 +0800

    引入log4js

--- bin/www.js
+++ bin/www.js
@@ -1,11 +1,30 @@
 #!/usr/bin/env node
 
-const debug = require('debug')('bee-blog:www');
+const fs = require('fs');
 const http = require('http');
-const app = require('../app/app');
+const log4js = require('log4js');
 
+/**
+ * make a log directory, just in case it isn't there.
+ */
+try {
+  fs.mkdirSync('./log');
+} catch (e) {
+  if (e.code !== 'EEXIST') {
+    // eslint-disable-next-line no-console
+    console.error('Could not set up log directory, ', e);
+    process.exit(1);
+  }
+}
+
+/**
+ * Initialise log4js first, so we don't miss any log messages
+ */
+log4js.configure('./config/log4js.json');
+
+const log = log4js.getLogger('startup');
 const port = process.env.PORT;
-const httpServer = http.createServer(app.callback());
+const httpServer = http.createServer(require('../app/app').callback());
 
 /**
  * Event listener for HTTP server "error" event.
@@ -22,15 +41,16 @@ function onError(err) {
   // handle specific listen errors with friendly messages
   switch (err.code) {
     case 'EACCES':
-      debug(`${bind} requires elevated privileges`);
+      log.info(`${bind} requires elevated privileges`);
       process.exit(1);
       break;
     case 'EADDRINUSE':
-      debug(`${bind} is already in use`);
+      log.info(`${bind} is already in use`);
       process.exit(1);
       break;
     default:
-      throw err;
+      log.info('unknown err', err);
+      process.exit(1);
   }
 }
 
@@ -42,7 +62,7 @@ function onListening() {
   const bind = typeof addr === 'string'
     ? `Pipe ${addr}`
     : `Port ${addr.port}`;
-  debug(`Listening on ${bind}`);
+  log.info('Server listening on port ', bind, ' with pid ', process.pid);
   // send the ready signal to PM2
   // process.send('ready');
 }
